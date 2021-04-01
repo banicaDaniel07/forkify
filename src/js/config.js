7b31b0037662388da92e82e177b7ea624173c308
banicaDaniel07 <banica.daniel07@gmail.com>
2021-04-01 18:04:31 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3292433c-46b7-40be-bc23-e568af71e2ab';
+export const MODAL_CLOSE_SEC = 2.5;
